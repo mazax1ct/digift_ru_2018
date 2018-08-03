51026e8a8240c5d9b47f7ff39a2effec8645a561
mazax1ct <vova@giveandget.ru>
2018-08-03 22:29:37 +0300

    articles slider

--- build/js/main.js
+++ build/js/main.js
@@ -1,5 +1,11 @@
 //изменение цвета меню
-var waypoints = $('.js-section-dark').waypoint(function(direction) {
+$('.js-section-dark').waypoint(function(direction) {
+  $('.main-menu').toggleClass('white');
+}, {
+  offset: '50'
+});
+
+$('.js-section-light').waypoint(function(direction) {
   $('.main-menu').toggleClass('white');
 }, {
   offset: '50'
@@ -28,4 +34,29 @@ $(document).ready(function() {
   $('.js-cards-slider').on('afterChange', function(event, slick, currentSlide, nextSlide){
     $(this).removeClass('no_pointer_events');
   });
+
+  //слайдер статей
+  if ($(".js-articles-slider").length) {
+    $('.js-articles-slider').each(function( index ) {
+      $(this).slick({
+        slidesToShow: 3,
+        infinite: false,
+        edgeFriction: 0,
+        prevArrow: '<button type="button" class="slick-prev" title="Назад"><img src="images/icons/slider_left.jpg"></button>',
+        nextArrow: '<button type="button" class="slick-next" title="Вперед"><img src="images/icons/slider_right.jpg"></button>',
+        dots: true,
+        appendDots: $('#'+ $(this).attr("data-dots") +'')
+      });
+    });
+  }
+
+  //убираем открытие карточек при перелистывании
+  $('.js-articles-slider').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+    $(this).addClass('no_pointer_events');
+  });
+
+  $('.js-articles-slider').on('afterChange', function(event, slick, currentSlide, nextSlide){
+    $(this).removeClass('no_pointer_events');
+  });
+
 });
